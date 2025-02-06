7047c775c79a4913eb136cccf0c5d82bd73c1098
Alejandro <alejandro.dulitzky@gmail.com>
2025-02-06 11:56:58 -0300

    Realizamos un contador de palabras con un pseudocógico, diagrama de flujo y códico python

---
+++ practica-algoritmos1.py
@@ -0,0 +1,23 @@
+#Diagrama de flujo a código python
+
+#Inicio del programa
+
+# Paso 1: Solicitar entrada al usuario
+# Mostrar mensaje: "Por favor, ingrese una palabra: "
+# Definir variable palabra_ingresada con la lectura de la entrada
+
+palabra_ingresada = input("Por favor, ingresa una palabra: ")
+
+
+# Paso 2: Contar la cantidad de letras que tiene la palabra
+# Definir la variable cantidad_letras con la lectura de la entrada (palabra_ingresada)
+
+cantidad_letras = len(palabra_ingresada)
+
+
+# Paso 3: Mostrar al usuario el resultado
+# Mostrar mensaje: "La palabra ingresada tiene", cantidad_letras, "letras."
+
+print("La palabra ingresada tiene", cantidad_letras, "letras")
+
+# Fin del programa
